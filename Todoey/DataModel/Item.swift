00996d684c2c6d3ea7c582377cd83ed5d37a623d
Yexiao Wu <yexiaowu@yexiaos-macbook-pro.local>
2019-02-14 20:26:28 -0600

    using codable protocol to persisting data

--- Todoey/DataModel/Item.swift
+++ Todoey/DataModel/Item.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-class Item{
+class Item : Codable{
 
     var m_title : String=""
     var m_done : Bool = false
--- Todoey/view controller/TodoListViewController.swift
+++ Todoey/view controller/TodoListViewController.swift
@@ -12,18 +12,12 @@ class TodoListViewController: UITableViewController {
 
     //data source
     var itemArray = [Item]()
-    
-    ///user default for default database
-    let userDefault = UserDefaults.standard
+    //data persis by creating our own plist instead of using the user default
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("TodoListItem.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //init data source from user default db
-        if let items = userDefault.array(forKey: "todoListArray") as? [Item]
-        {
-            itemArray = items
-        }
+        loadItems()
     }
     
     //table view data source methods
@@ -43,26 +37,56 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         itemArray[indexPath.row].m_done = !itemArray[indexPath.row].m_done
+        saveItems()
         tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     //add item
     @IBAction func AddButtonPressed(_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "Add Item To the List", message: "", preferredStyle: .alert)
-        let newItem = Item(title: "", done: false)
+        
+        
+        var textField = UITextField()
+        
         alert.addTextField { (newTextField) in
             newTextField.placeholder = "create new item"
-            newItem.m_title =  newTextField.text!
+            textField = newTextField
         }
         
         let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            
             //what will happen when button pressed
+            let newItem = Item(title: textField.text!, done: false)
             self.itemArray.append(newItem)
-            self.userDefault.set(self.itemArray, forKey: "todoListArray")
+           
+            self.saveItems()
             self.tableView.reloadData()
+            
         }
+        
         alert.addAction(action)
         present(alert,animated: true,completion: nil)
+    }
+    func saveItems()
+    {
+        let encoder = PropertyListEncoder()
+        do{
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)
+        }catch{
+            print("Error encoding item array, \(error)");
+        }
+        
+    }
+    func loadItems() {
+       if let data = try? Data(contentsOf: dataFilePath!){
+        let decoder = PropertyListDecoder()
+        do{
+            itemArray = try decoder.decode([Item].self, from: data)
+        }catch{
+            print("error decoding, \(error)")
+        }
+        }
     }
     
 }
