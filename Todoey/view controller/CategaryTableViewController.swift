bbf4229a9ea3c1f194644629f0212b1da3fb67e6
Yexiao Wu <yexiaowu@yexiaos-macbook-pro.local>
2019-02-19 22:13:50 -0600

    create category table view and table view controller

---
+++ Todoey/view controller/CategaryTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CategaryTableViewController.swift
+//  Todoey
+//
+//  Created by Yexiao Wu on 2019-02-19.
+//  Copyright © 2019 Yexiao Wu. All rights reserved.
+//
+
+import UIKit
+
+class CategaryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    @IBAction func addButtonPress(_ sender: UIBarButtonItem) {
+    }
+}
